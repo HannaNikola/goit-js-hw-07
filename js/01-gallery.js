14f72487b7319bf99014bdd4d7459d6b5c4b931f
HannaNikola <h.nikolaienko32@gmail.com>
2023-07-11 13:54:47 +0200

    change- photo

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -37,7 +37,7 @@ const imagesList = galleryItems.map((item) => {
         `
 }).join('');
 
-document.body.insertAdjacentHTML('beforeend', imagesList);
+imgEl.insertAdjacentHTML('beforeend', imagesList);
 console.log(imagesList);
 
 
